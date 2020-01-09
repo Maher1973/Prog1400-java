5295c0f74fb0b57db8ae80306aa9387b59d5ccbf
Maher <maherhelwany@yahoo.com>
2020-01-09 11:52:45 -0400

    maher

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,12 @@
+
+
+class HelloWorld {
+
+    public static void main(String[] args) {
+
+        // print out hello
+        System.out.println("Hello World");
+        System.out.println("nscc"); // extra line
+        }
+
+    }
