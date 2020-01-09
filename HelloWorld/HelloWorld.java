362bc96086ab5562a414c9dd3078caf0bf5469bc
Maher <maherhelwany@yahoo.com>
2020-01-09 12:18:37 -0400

    Change to print number

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -4,11 +4,17 @@ class HelloWorld {
 
     public static void main(String[] args) {
 
-        // print out hello
+        int num;
+        num = 123456;
+
+
+        // print out num
+        System.out.println("num =" + num);
+        // print Hello
         System.out.println("Hello World");
         System.out.println("nscc"); // extra line
         System.out.println("Prog1400");
-        System.out.println("Maher");
+
         }
 
     }
